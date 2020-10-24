c63ef643ad5a3fd6279b184c2eb7cf1b5a486a66
pythonstudent01 <python-student@den01.co.uk>
2020-10-24 10:36:33 +0000

    Attempt solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #write your code below this line
+    with open("data.txt",'r') as f:
+      data = f.read().splitlines()
+    
+    for row in data:
+      print(row)
 
 if __name__ == '__main__':
     main()
